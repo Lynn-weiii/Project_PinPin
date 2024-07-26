12b26bebce48c3d21445e6be138626512fa1e9d1
Bao <baopeiyi30@gmail.com>
2024-07-26 19:05:18 +0800

    feat.schedule畫面新增輪播及tabs --google place api搜尋地點回傳經緯度 --schedule group要新增一個欄位是isHoster，這樣才能辨別是我的行程還是旅遊群組

---
+++ RegisterLoginClient/wwwroot/js/coursescard.js
@@ -0,0 +1,75 @@
+﻿function createCourseItem(course) {
+    var courseItem = document.createElement('div');
+    courseItem.className = 'item course_card_owl_item';
+    courseItem.innerHTML = `
+    <a href="#" class="course_card_link">
+       <div class="course_card">
+            <div class="course_card_container">
+                <div class="course_card_top">
+                    <div class="course_card_category trans_200"></div>
+                    <div class="course_card_pic">
+                        <img src="/images/caourse/course_03.jpg">
+                    </div>
+                    <div class="course_card_content">
+                        <div class="course_card_meta d-flex flex-row align-items-center"></div>
+                        <div class="course_card_title">
+                            <h3>${course.name}</h3>
+                        </div>
+                        <div class="course_card_author">
+                            <span>by ${course.userName}</span>
+                        </div>
+                        <div class="course_card_rating d-flex flex-row align-items-center">
+                            <h5>${course.startTime}</h5>
+                            <h5 style="padding:5px;"><i class="fa-solid fa-arrow-right" style="color: #0e4e3b;"></i></h5>
+                            <h5>${course.endTime}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    `;
+    return courseItem;
+}
+
+function renderCourses(data) {
+    var container = document.getElementById('course-container');
+    container.innerHTML = ''; // Clear existing content
+
+    data.forEach(course => {
+        var courseItem = createCourseItem(course);
+        container.appendChild(courseItem);
+    });
+
+    // Create the "Add New" card after all courses have been rendered
+    var addNewCard = document.createElement('div');
+    addNewCard.className = 'item course_card_owl_item';
+    addNewCard.innerHTML = `
+    <div class="course_card" id="addnewcard">
+        <div class="course_card_container">
+            <br><br><br><br>
+            <div class="course_card_pic" style="py-5">
+                <a href="javascript:;" class="add_btn" data-bs-toggle="modal" data-bs-target="#newschdule">
+                    <img src="/images/add_new.png" alt="Add New">
+                </a>
+                <br><br><br><br>
+            </div>
+        </div>
+    </div>
+    `;
+    container.appendChild(addNewCard);
+
+    initPopularCoursesSlider();
+}
+
+function serachCourses(data) {
+    var container = document.getElementById('course-container');
+    container.innerHTML = ''; // Clear existing content
+
+    data.forEach(course => {
+        var searchItem = createCourseItem(course);
+        container.appendChild(searchItem);
+    });
+
+    initPopularCoursesSlider();
+}
--- RegisterLoginClient/wwwroot/js/popular_courses_slider.js
+++ RegisterLoginClient/wwwroot/js/popular_courses_slider.js
@@ -1,68 +1,92 @@
-﻿function initPopularCoursesSlider() {
-	if ($('.popular_courses_slider').length) {
-		let slider = $('.popular_courses_slider');
-		let item_count;
-		let item_index;
-		let page_size;
+﻿//function initPopularCoursesSlider() {
+//	if ($('.popular_courses_slider').length) {
+//		let slider = $('.popular_courses_slider');
+//		let item_count;
+//		let item_index;
+//		let page_size;
 
-		let prev = slider.siblings('.slider_nav_btn_prev');
-		let next = slider.siblings('.slider_nav_btn_next');
+//		let prev = slider.siblings('.slider_nav_btn_prev');
+//		let next = slider.siblings('.slider_nav_btn_next');
 
-		slider.owlCarousel(
-			{
-				loop: false,
-				margin: 24,
-				nav: false,
-				dots: false,
-				responsive:
-				{
-					0: {
-						items: 1
-					},
-					768: {
-						items: 2
-					},
-					1200: {
-						items: 3
-					}
-				},
-				onInitialized: on_change,
-				onChanged: on_change
-			});
+//		slider.owlCarousel(
+//			{
+//				loop: false,
+//				margin: 24,
+//				nav: false,
+//				dots: false,
+//				responsive:
+//				{
+//					0: {
+//						items: 1
+//					},
+//					768: {
+//						items: 2
+//					},
+//					1200: {
+//						items: 3
+//					}
+//				},
+//				onInitialized: on_change,
+//				onChanged: on_change
+//			});
 
-		function on_change(event) {
-			item_count = event.item.count;
-			page_size = event.page.size;
-			item_index = event.item.index + 1;
+//		function on_change(event) {
+//			item_count = event.item.count;
+//			page_size = event.page.size;
+//			item_index = event.item.index + 1;
 
-			// Show "back" button when you slide forward at least once
-			if (item_index > 1) {
-				prev.removeClass('disabled');
-			}
-			else {
-				prev.addClass('disabled');
-			}
+//			// Show "back" button when you slide forward at least once
+//			if (item_index > 1) {
+//				prev.removeClass('disabled');
+//			}
+//			else {
+//				prev.addClass('disabled');
+//			}
 
-			// Hide "next" navigation button when there's no more slides to show
-			if (item_index === (item_count - page_size + 1) || item_count < page_size) {
-				next.addClass('disabled');
-			}
-			else {
-				next.removeClass('disabled');
-			}
-		}
+//			// Hide "next" navigation button when there's no more slides to show
+//			if (item_index === (item_count - page_size + 1) || item_count < page_size) {
+//				next.addClass('disabled');
+//			}
+//			else {
+//				next.removeClass('disabled');
+//			}
+//		}
 
-		// Slider navigation buttons
-		if (prev.length) {
-			prev.on('click', function () {
-				slider.trigger('prev.owl.carousel');
-			});
-		}
+//		// Slider navigation buttons
+//		if (prev.length) {
+//			prev.on('click', function () {
+//				slider.trigger('prev.owl.carousel');
+//			});
+//		}
 
-		if (next.length) {
-			next.on('click', function () {
-				slider.trigger('next.owl.carousel');
-			});
-		}
-	}
+//		if (next.length) {
+//			next.on('click', function () {
+//				slider.trigger('next.owl.carousel');
+//			});
+//		}
+//	}
+//}
+
+function initPopularCoursesSlider() {
+    if ($('.popular_courses_slider').length) {
+        let slider = $('.popular_courses_slider');
+
+        slider.owlCarousel({
+            loop: false,
+            margin: 24,
+            nav: false,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                1200: {
+                    items: 3
+                }
+            }
+        });
+    }
 }
