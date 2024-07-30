7d89687f5736b11917bce908fb13a8ef0bad9e66
Lynn-weiii <104101089@mail2.nccu.tw>
2024-07-30 09:28:35 +0800

    feature:新增側邊欄 -僅新增畫面框架，內容待補

---
+++ IdentityJWT/Controllers/WishlistController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PinPinServer.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PinPinServer.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishlistController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly PinPinContext _context;
+
+        public WishlistController(PinPinContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // GET: api/<WishlistController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<WishlistController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<WishlistController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WishlistController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WishlistController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
